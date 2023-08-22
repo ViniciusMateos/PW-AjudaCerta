b2c068edaa3c8bea30a0dabbe3b61640c2966193
Vitor <vitorsouzalibrelonrestini@gmail.com>
2023-08-22 10:36:58 -0300

    Começo do projeto

---
+++ src/main/java/br/com/etechoracio/Ajuda/Certa/AjudaCertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.Ajuda.Certa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaCertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaCertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/Ajuda/Certa/AjudaCertaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.Ajuda.Certa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjudaCertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
